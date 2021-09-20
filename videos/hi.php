05b348a38ac2668be24022572e958285329b0f00
INF3RNOCAT <86680605+INF3RNOCAT@users.noreply.github.com>
2021-09-20 18:48:21 -0400

    Update hi.php

--- videos/hi.php
+++ videos/hi.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/svg+xml" href="icon.svg">
-        <meta name="description" content="A list of all videos.">
-        <meta name="theme-color" content="#FF0000">
-        <title>Video Directory</title>
-    </head>
-    <body>
-        <h1>videos are:</h1>
-    </body>
-</html>
+f
