5d7ca9cd974be8fcd88e7f0d78b937736ab9affe
INF3RNOCAT <86680605+INF3RNOCAT@users.noreply.github.com>
2021-10-12 22:20:38 -0400

    Create video-array.js

---
+++ video-array.js
@@ -0,0 +1,11 @@
+// testing something, i don't remember everything. FUCK YOU.
+
+var videoArray = [];
+
+export function getArray() {
+  return videoArray
+}
+
+export function getRandomVideo() {
+  return videoArray[Math.floor(Math.random() * videoArray.length)]
+}
