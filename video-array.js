ffac8d4533f7bacfffce33c18a303ba08d026d46
SuperSpaceMan230 <61877191+SuperSpaceMan230@users.noreply.github.com>
2021-12-02 22:48:02 +0100

    Update video-array.js

--- video-array.js
+++ video-array.js
@@ -826,6 +826,7 @@ var videoArray = [
   'videos/Spanish_Skeleton.mp4',
   'videos/sponb.mp4',
   'videos/sponbg.mp4',
+  "videos/Tainted_Lost.mp4",
   'videos/team_fort_two.mp4',
   'videos/The_Isaac_of_Isaac_-_ReIsaac.mp4',
   'videos/wayfair.mp4',
@@ -1123,6 +1124,7 @@ var videoArray = [
   'videos/yodazo.mp4', //EM
   'videos/fairy_flip.mp4', //AND
   'videos/8008135.mov' //WEEP
+
   //
 ]
 
