b6b0dfcecea8aedd5acd00cfbcce0af77e2aeaad
maumaumaumaumaumau <maumaumaumaumaumua@gmail.com>
2023-02-16 00:10:40 -0300

    Update v-array.js

--- functions/v-array.js
+++ functions/v-array.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 let fileRegex = /.mp4|.mov|.ogv|.webm|.flv|.vob|.ogv|.drc|.gifv|.mng|.avi|.mov|.qt|.mts|.m2ts|.ts|.wmv|.yuv|.rm|.rm|.rmv|.viv|.asf|.amv|.m4p|.m4v|.mpg|.mp2|.mpeg|.mpe|.mpv|.m2v|.m4v|.svi|.3gp|.3g2|.mxf|.roq|.nsv|.f4v|.f4vp|.f4a|.f4b/
 
-exports.handler = function() {
+exports.handler = async function(event, context) {
   let files = []
   let dir = fs.readdirSync("/", { withFileTypes: true })
   
