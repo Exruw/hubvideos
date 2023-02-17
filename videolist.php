5d5fc7baf0e9c816491e76b89a8dd9ecbd439c78
Fuck. You <85572294+Exruw@users.noreply.github.com>
2023-02-16 21:50:08 -0500

    delete stuff

--- videolist.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-type: application/json; charset=utf-8');
-
-$dir    = './flist';
-$files1 = array_slice(scandir($dir),2);
-$exclusion = array("\.\.");
-$list = json_encode($files1, JSON_PRETTY_PRINT);
-print($list);
-?>
